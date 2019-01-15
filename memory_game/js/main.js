6323bc1420e6febe97cd84ccb2561df2b9d2a59b
Mandy Sommerfield <mandysommerfield@Mandys-MacBook.local>
2019-01-15 12:42:40 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,17 +1,26 @@
 console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"] ;
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = cards[0];
+var cardTwo = cards[1];
+var cardThree = cards[2];
+var cardFour = cards[3];
 
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
 
 
-var cardsInPlay = [cards.push('cardOne'), cards.push('cardTwo') ];
-var cardOne = cards[0]; 
+var cardsInPlay = [cards.push('cardOne'), cards.push('cardTwo') ]
+if (cardsInPlay === 2) {cardsInPlay.length === 2};
+else if (cardsInPlay[0] === cardsInPlay[1] ) {
+	alert("You found a match!");
+}
+else (cardsInPlay[0] !=== cardsInPlay[1]) {
+	alert("Sorry, try again.")
+}
+
+
+
 console.log( "user flipped " + cardOne) ;
 
 var cardTwo = cards[2]; 
