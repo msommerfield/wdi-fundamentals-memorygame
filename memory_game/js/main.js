dbbf7f6e40bbd6ff20aa463a39155b36d7920676
Mandy Sommerfield <mandysommerfield@Mandys-MacBook.local>
2019-01-16 17:16:59 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,45 +1,67 @@
-console.log("Up and running!");
-
-var cards = ["queen", "queen", "king", "king"] ;
-
-var cardOne = cards[0];
-var cardTwo = cards[1];
-var cardThree = cards[2];
-var cardFour = cards[3];
-
-var cardsInPlay = [];
-
-var checkForMatch = function () {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
-	} else {
-	console.log("Sorry, try again.");
+var cards = [
+ {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png",
+  id: 0
+ },
+ {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png",
+  id: 1
+ },
+ {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png",
+  id: 2
+},
+{
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png",
+  id: 3
+}
+];
+//1
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    // console.log(cardElement);
+    cardElement.addEventListener('click', (function(x) {return function() {flipCard(x)}})(i)); // 'i' is immediately evaluated to the correct value
+    cards[i].element = cardElement; // Keep association with DOM here
 	}
 }
 
+createBoard();
+//2
+function flipCard () {
+  var cardId = this.getAttribute('data-id');
+  cardsInPlay.push(cards[cardId].rank);
+  cardsInPlay.push(cards[cardId].id);
+  this.setAttribute('src', cards[cardId].cardImage);
+  console.log("User flipped " + cards[cardId].rank);
+  console.log("cardImage");
+  console.log("suit");
+
+// Check for match here =>
+  if (cardsInPlay.length === 2) {
+  	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		alert("You found a match!");
+  	}
+  	else {
+  		alert("Sorry, try again.");
+  		console.log(cardsInPlay);
+  		cardsInPlay[0].setAttribute('src', 'images/back.png'); // this doesnt work
+  	}   cardsInPlay[1].setAttribute('src', 'images/back.png'); //this doesnt work
+  }
+
+var cardsInPlay = [] ;
 
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
 
-var cardTwo = cards[2]; 
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
 
-var flipCard = function (cardID) {
-	if (cardsInPlay === 2) {cardsInPlay.length === 2}; {
-		return true;
-	} else if { (cardsInPlay[0] === cardsInPlay[1] ) {
-	alert("You found a match!") }
-	} else {(cardsInPlay[0] !=== cardsInPlay[1]) {
-	alert("Sorry, try again.") }
-	console.log("User flipped" + cards[cardID]);
-};
-
-flipCard(0);
-flipCard(2);
-checkForMatch();
 
 
 
